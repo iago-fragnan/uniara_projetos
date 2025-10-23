362f744c1d617e6c5b6300c37f855b25e7ce32d7
Iago F. <n3rdydzn@hotmail.com>
2025-10-23 15:51:17 -0300

    exercicios que estava faltando

--- algoritmos/21-10-25/exercicio_2.c
+++ algoritmos/21-10-25/exercicio_2.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#define size 30
 
 int main()
 {
+    int valores[size];
+
+    int pares[size];
+    int impares[size];
+
+    int indice = 0;
+
+    while(indice < size) {
+        int add = 0;
+        printf("Digite um numero: \n> ");
+        scanf("%d", &add);
+        valores[indice] = add;
+
+        indice++;
+    }
+
+    for(int i = 0; i < size; i++){
+        if (valores[i] % 2 == 0){
+            pares[i] = valores[i];
+            impares[i] = 0;
+        } else {
+            impares[i] = valores[i];
+            pares[i] = 0;
+        }
+    }
+
+    int tam_pares = 0;
+    int tam_impares = 0;
+
+    for(int j = 0; j < size; j++){
+        if (pares[j] != 0){
+            tam_pares++;
+        }
+        if (impares[j] != 0){
+            tam_impares++;
+        }
+    }
+
+    printf("Total de pares: %d\n", tam_pares);
+    printf("Total de impares: %d\n", tam_impares);
+
     return 0;
 }
--- algoritmos/21-10-25/exercicio_3.c
+++ algoritmos/21-10-25/exercicio_3.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
+#define max_size 10
+
 int main()
 {
+    float valoresA[max_size];
+    float valoresB[max_size];
+    int indice = 0;
+
+    while(indice < (max_size*2)) {
+        if(indice < max_size){
+            float addA = 0;
+            printf("Digite o valor A:\n>");
+            scanf("%f", &addA);
+            valoresA[indice] = addA;
+        }
+        else {
+            float addB = 0;
+            printf("Digite o valor B:\n>");
+            scanf("%f", &addB);
+            valoresB[indice - max_size] = addB;
+        }
+        indice++;
+    }
+
+    for(int i = 0; i < max_size; i++){
+        if(valoresA[i] < valoresB[i]){
+            printf("%.2f é menor que %.2f, B GANHOU\n", valoresA[i], valoresB[i]);
+        }
+        else if(valoresA[i] > valoresB[i]){
+            printf("%.2f é maior que %.2f, A Ganhou\n", valoresA[i], valoresB[i]);
+        }
+        else {
+            printf("%.2f é igual a %.2f, EMPATE\n", valoresA[i], valoresB[i]);
+        }
+    }
+
     return 0;
 }
